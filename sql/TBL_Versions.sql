da8ac42c58880beba540e09f6fa8c2550f7f2ee1
Miroslav Bozik <miroslav.bozik@sunshineis.com>
2019-02-27 23:44:22 +0700

    #38 adding sql file

---
+++ sql/TBL_Versions.sql
@@ -0,0 +1,12 @@
+-- Table Versions
+CREATE TABLE Versions (
+	Id int NOT NULL,
+	Description nvarchar(200) NOT NULL,
+	TimeUTC datetime NOT NULL,
+
+	CONSTRAINT [PK_Versions] PRIMARY KEY (Id)
+)
+
+-- Index on column TimeUTC
+CREATE INDEX IDX_Versions_TimeUTC
+ON Versions (TimeUTC)
